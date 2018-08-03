39423c575edb655759b591546b5a2ba75937a20e
Hector Ivan <gho0thubun@gmail.com>
2018-08-02 19:38:51 -0500

    Configuracion con webpack

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app');
+const hola = (<h1>Prro salvenme</h1>);
+ReactDOM.render(hola, container);
